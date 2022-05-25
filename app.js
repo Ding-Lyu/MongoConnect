72d13bf8be43243ebe46d889dc9c11c0275b377c
Ding-Lyu <lyudingguangnz@outlook.com>
2022-05-25 15:24:11 +1200

    Mongo connection

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+
+//connect to mongoDB
+
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/exampleDB")
+  .then(() => {
+    console.log("Connected to MongoDB.");
+  })
+  .catch((err) => {
+    console.log("Connection failed.");
+    console.log(err);
+  });
+
+//Define a schema,
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String,
+  scholarship: {
+    merit: Number,
+    other: Number,
+  },
+});
+//create a model for students
+const Student = mongoose.model("Student", studentSchema);
+
+//model mongo Shell
+//create an object
+const Xu = new Student({
+  name: "Concordia Xu",
+  age: 25,
+  major: "BA",
+  scholarship: { merit: 2500, other: 1300 },
+});
+
+//save Xu into database
+Xu.save()
+  .then(() => {
+    console.log("Concordia has been save in the database");
+  })
+  .catch((e) => {
+    console.log("error happend.");
+    console.log(e);
+  });
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
